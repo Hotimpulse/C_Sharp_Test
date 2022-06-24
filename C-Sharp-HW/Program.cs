cae63cd5326cf85a771cf8d0bc4fefea8e009ed1
Hotimpulse <jz2@rambler.ru>
2022-06-24 22:24:58 +0300

    update with task 6 and task 8

--- C-Sharp-HW/Program.cs
+++ C-Sharp-HW/Program.cs
@@ -1,4 +1,5 @@
-﻿// Console.WriteLine("Enter first number: ");
+﻿// Task 2.
+// Console.WriteLine("Enter first number: ");
 // int numberOne = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Enter second number: ");
 // int numberTwo = Convert.ToInt32(Console.ReadLine());
@@ -10,33 +11,52 @@
 // 	Console.WriteLine("The bigger number is " + numberTwo);
 // }
 
+// Task 4.
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         Console.WriteLine("You are about to enter three numbers and find the largest. \n");
+
+//         Console.WriteLine("Enter the first number: ");
+//         int a = Convert.ToInt32(Console.ReadLine());
+//         Console.WriteLine("Enter the second number: ");
+//         int b = Convert.ToInt32(Console.ReadLine());
+//         Console.WriteLine("Enter the third number: ");
+//         int c = Convert.ToInt32(Console.ReadLine());
+
+//         if (a > b && a > c)
+//         {
+//             Console.WriteLine("The largest number is " + a);
+//         }
+//         else if (b > a && b > c) {
+//                 Console.WriteLine("The largest number is " + b);
+//             }
+//         else if (c > a && c > b) {
+//             Console.WriteLine("The largest number is " + c);
+//         }
+//     }
+// }
+
+//Task 6.
+
+// Console.WriteLine("Enter a number to check if it can be divided by two: ");
+// int anyNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (anyNumber % 2 == 0) {
+//     Console.WriteLine("Divisable by 2");
+// } else {
+//     Console.WriteLine("Not divisable by 2");
+// }
+
+// Task 8.
 
 
+// Console.WriteLine("Enter a number and see all even numbers that are less or equal to your number: ");
+// int number = Convert.ToInt32(Console.ReadLine()); 
+// for (int i = 1; i <= number; i++) {
+//     if (i%2==0) {
+//         Console.WriteLine("All even numbers that are less or equal to your number: " + i);
+//     }
+// }
 
-
-
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("You are about to enter three numbers and find the largest. \n");
-
-        Console.WriteLine("Enter the first number: ");
-        int a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter the second number: ");
-        int b = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter the third number: ");
-        int c = Convert.ToInt32(Console.ReadLine());
-
-        if (a > b && a > c)
-        {
-            Console.WriteLine("The largest number is " + a);
-        }
-        else if (b > a && b > c) {
-                Console.WriteLine("The largest number is " + b);
-            }
-        else if (c > a && c > b) {
-            Console.WriteLine("The largest number is " + c);
-        }
-    }
-}
