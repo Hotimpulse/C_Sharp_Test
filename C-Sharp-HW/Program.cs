3328a0ff80fdeeeb79098bc529f8577f2849176e
Hotimpulse <jz2@rambler.ru>
2022-06-24 18:43:32 +0300

    add task 1

---
+++ C-Sharp-HW/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter first number: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+if (numberOne > numberTwo) {
+	Console.WriteLine(numberOne);
+}
+else {
+	Console.WriteLine("The bigger number is " + numberTwo);
+}
